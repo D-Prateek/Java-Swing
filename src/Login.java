d2ea32e428d1b23fe64ca13d28f028f68ce871b1
D-Prateek <pratikdangal22@gmail.com>
2026-01-31 15:41:38 +0545

    first

---
+++ src/Login.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Login extends JFrame{
+    JLabel firstname, lastname;
+    JTextField txtfirst, txtsecond;
+
+    public Login(){
+        JFrame frame= new JFrame("Workspace");
+        frame.setSize(1200,800);
+    }
+}
