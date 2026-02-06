cc19c98c4290ae387815285a4772db1cd3a7a3d7
D-Prateek <pratikdangal22@gmail.com>
2026-02-06 18:25:20 +0545

    LoginC

--- src/Login.java
+++ src/Login.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Login extends JFrame {
     JLabel username, password;
@@ -11,11 +13,21 @@ public class Login extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(null);
 
+        JPanel panel=new JPanel();
+        panel.setLayout(null);
+        panel.setBounds(350,200,500,300);
+
         username = new JLabel("Username");
         password = new JLabel("Password");
 
         txtfirst = new JTextField();
         txtsecond = new JPasswordField();
+
+        JButton loginbtn= new JButton("Login");
+        JButton Signup= new JButton("Signup");
+
+        JToggleButton eyeButton =new JToggleButton ("👁️");
+        eyeButton.setFocusPainted(false);
 
         username.setBounds(400, 250, 100, 30);
         txtfirst.setBounds(500, 250, 200, 30);
@@ -23,11 +35,28 @@ public class Login extends JFrame {
         password.setBounds(400, 300, 100, 30);
         txtsecond.setBounds(500, 300, 200, 30);
 
-        add(username);
-        add(txtfirst);
-        add(password);
-        add(txtsecond);
+        loginbtn.setBounds(380,350,100,30);
+        Signup.setBounds(600,350,100,30);
 
+        eyeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (eyeButton.isSelected()) {
+                    txtsecond.setEchoChar((char)0);
+                }
+                else{
+                    txtsecond.setEchoChar('*');
+                }
+            }
+        });
+
+        panel.add(username);
+        panel.add(txtfirst);
+        panel.add(password);
+        panel.add(txtsecond);
+        panel.add(loginbtn);
+        panel.add(Signup);
+        add(panel);
         setVisible(true);
     }
 }
